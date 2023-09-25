51ce2a64a5940f3506c057e8122b6c51cbd534a2
emmarosenlind <emma.rosenlind@gmail.com>
2023-09-25 10:43:33 +0900

    Delete video in background

--- src/Containers/About/About.jsx
+++ src/Containers/About/About.jsx
@@ -9,7 +9,6 @@ function About(){
   return (
     <div className='portfolio' id ="about">
       <div className="overlay"></div>
-       <video autoPlay loop muted id = "video"><source src={video}></source></video>
        <div className='portfolio__about' id = "about">
           <div className ="portfolio__about-content">
               <h1>It's all about <br/> the experience</h1>
