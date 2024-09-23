25165a1a4995e63e5204f255c96200ca89e6dabc
emmarosenlind <emma.rosenlind@gmail.com>
2024-09-23 16:29:46 +0200

    Update Header.jsx

--- src/Containers/Header/Header.jsx
+++ src/Containers/Header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
   return (
     <div className ="portfolio__header section-padding" id="home">
       <div className="portfolio__header-content">
-        <h1 className="gradient__text">Hi, I am Emma, a UX-designer from Sweden who loves...</h1>
+        <h1 className="gradient__text">Hi, I am Emma, an UX/UI-designer from Sweden who loves...</h1>
         <p>Making boring things exciting, skiing down mountains, developing new skills, and creating designs that will make your heart skip a beat. Let's make something fun together!</p>
         <div className ="portfolio__header-content__links">
           <p><a href="https://www.linkedin.com/in/emmarosenlind/">Linkedin</a></p>
